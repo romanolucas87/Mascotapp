acaf8ff9a69f6a4280cafc0e3039125828c11aba
Lucas Romano <romanolucas87@gmail.com>
2023-07-27 20:35:31 -0300

    se importó boostrap icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
-import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
+import NavBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
 
 function App() {
   return (
     <>   
-     <Navbar />  
-      {/* Agrega aquí el contenido principal de tu aplicación */}
+     <NavBar />  
+     <ItemListContainer greetings="Bienvenidos"/>
   
 
     </>
