4b57ca7ccf8c8b3b77badd6212af3a90860d0e3e
Lucas Romano <romanolucas87@gmail.com>
2023-07-31 22:55:07 -0300

    se importó funcionalidad de suma y resta de items en itemListConteiner junto con su Boton

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,7 @@
 import { Cart3 } from "react-bootstrap-icons";
 
 
-function CartWidget({item =''}) {
+function CartWidget({item}) {
     return(
         <div className="text-center mt-4 mb-4">
             <Cart3 size={25}/>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,19 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { ItemCount } from './ItemCount';
 
 
 
 function ItemListContainer ({greetings}){
+  const onAdd = (quantity)=>{
+    console.log(`Compraste ${quantity} unidades`);
+  }
     return (
         <Container className='text-center'>
           <Row >
             <Col><h1>{greetings} </h1></Col>
+            <ItemCount initial={1} stock={9} onAdd={onAdd}/>
           </Row>
         </Container>
       );
