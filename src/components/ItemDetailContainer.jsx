7960c82f389d4d45fe55c3c9f6c455a310d44774
Lucas Romano <romanolucas87@gmail.com>
2023-09-07 17:04:38 -0300

    Se obtienen los datos de firebase, y se agregaron metodos para filtrar por id producto.

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,30 +1,30 @@
 import ItemDetail from "./ItemDetail"
 import { useState, useEffect } from "react";
-import { getProduct } from '../AsyncMock';
 import Loader from './Loader';
 import { useParams } from "react-router-dom";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection,getDocs } from "firebase/firestore";
 import { firestore } from "../firebase/config";
+
 export const ItemDetailContainer = () =>{
     
     const[product, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const productID = useParams();
      useEffect(()=>{
+        console.log('produid', productID);
 
-  const collectionRef = collection(firestore, "productsPets");
-  getDocs(collectionRef).then(response => setProducts({id: response.id, ...response.data() })).catch(error => console.log(error)).finally(()=>setIsLoading(false));    
- }, [])
-    // useEffect(() => {
-    //     getProduct(productID.id)
-    //     .then( result => {console.log(result);
-    //             setProduct({...result})})
-    //     .catch(error => console.error(error))
-    //     .finally(()=> setIsLoading(false));
-
-
-    // }, [productID]
-    // )
+        const queryProducts = 
+            collection(firestore, "productsPets")         
+          getDocs(queryProducts)
+            .then((snapshot) => { console.log('snap' , snapshot)
+            const products =snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })); console.log('prod', products);
+            const producto = products.find(product => product.id === productID.id); console.log('producto', producto);
+            setProducts(products.find(product => product.id == productID.id))
+            })
+            .catch((e) => console.error(e))
+            .finally(() => setIsLoading(false));    
+    }, [productID])
+    
     if(isLoading) return <Loader/>;
 
     // return(
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,34 +1,52 @@
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 import ItemList from "./ItemList";
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { getProducts } from '../AsyncMock';
-import Loader from './Loader';
+import { getProducts } from "../AsyncMock";
+import Loader from "./Loader";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { firestore } from "../firebase/config";
 
- const ItemListContainer = () =>{
-    const [products, setProducts]= useState([]);
-    const params = useParams();
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => { 
-      getProducts()
-        .then((response) => {
-          params.id ? setProducts(response.filter(product => product.category == params.id)) : setProducts(response)          
-        })
-        .catch(error => console.error(error))
-        .finally(()=> setIsLoading(false));     
-      
-    }, [params]);
-    if(isLoading) return <Loader/>;
-    return (
-      <>
-        <Container className='text-center'>         
-          <Row className='m-3'> 
-           <ItemList productList = {products}></ItemList>         
-          </Row>          
-        </Container>
-        </>
-      );
-    }
+const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+  const params = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    console.log('params ', params.id);
+    const queryProducts = params.id ? query(
+      collection(firestore, "productsPets"),
+      where("category", "==", params.id)
+    ):  query(
+      collection(firestore, "productsPets")
+    ) 
+    getDocs(queryProducts)
+      .then((snapshot) => {
+        setProducts(snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  // useEffect(() => {
+  //   getProducts()
+  //     .then((response) => {
+  //       params.id ? setProducts(response.filter(product => product.category == params.id)) : setProducts(response)
+  //     })
+  //     .catch(error => console.error(error))
+  //     .finally(()=> setIsLoading(false));
+
+  // }, [params]);
+  if (isLoading) return <Loader />;
+  return (
+    <>
+      <Container className="text-center">
+        <Row className="m-3">
+          <ItemList productList={products}></ItemList>
+        </Row>
+      </Container>
+    </>
+  );
+};
 
 export default ItemListContainer;
