790695374335ac57463ccfa15143f29546acbdd9
Lucas Romano <romanolucas87@gmail.com>
2023-09-06 23:02:07 -0300

    se obtienen los datos de firebase

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -3,24 +3,28 @@ import { useState, useEffect } from "react";
 import { getProduct } from '../AsyncMock';
 import Loader from './Loader';
 import { useParams } from "react-router-dom";
-
-
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { firestore } from "../firebase/config";
 export const ItemDetailContainer = () =>{
     
-    const[product, setProduct] = useState([]);
+    const[product, setProducts] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const productID = useParams();
+     useEffect(()=>{
 
-    useEffect(() => {
-        getProduct(productID.id)
-        .then( result => {console.log(result);
-                setProduct({...result})})
-        .catch(error => console.error(error))
-        .finally(()=> setIsLoading(false));
+  const collectionRef = collection(firestore, "productsPets");
+  getDocs(collectionRef).then(response => setProducts({id: response.id, ...response.data() })).catch(error => console.log(error)).finally(()=>setIsLoading(false));    
+ }, [])
+    // useEffect(() => {
+    //     getProduct(productID.id)
+    //     .then( result => {console.log(result);
+    //             setProduct({...result})})
+    //     .catch(error => console.error(error))
+    //     .finally(()=> setIsLoading(false));
 
 
-    }, [productID]
-    )
+    // }, [productID]
+    // )
     if(isLoading) return <Loader/>;
 
     // return(
